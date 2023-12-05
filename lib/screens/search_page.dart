210d415310de439acd16fa47096ba3d3c7e93337
Melika2082 <144673399+Melika2082@users.noreply.github.com>
2023-12-05 20:26:12 +0330

    Add files via upload

---
+++ lib/screens/search_page.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+
+class SearchPageTextField extends StatelessWidget {
+  const SearchPageTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Search Page'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              showSearch(
+                context: context,
+                delegate: CustomSearch(),
+              );
+            },
+            icon: const Icon(Icons.search),
+          )
+        ],
+      ),
+      body: const SafeArea(
+        child: Center(),
+      ),
+    );
+  }
+}
+
+class CustomSearch extends SearchDelegate {
+  List<String> allData = [
+    'Python',
+    'Flutter',
+    'HTML',
+    'CSS',
+    'Java',
+    'Javascript',
+    'Kotlen',
+  ];
+
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+        onPressed: () => query = '',
+        icon: const Icon(Icons.clear),
+      )
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+      onPressed: () {
+        close(context, null);
+      },
+      icon: const Icon(Icons.arrow_back),
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    List<String> matchQuery = [];
+    for (var item in allData) {
+      if (item.toLowerCase().contains(query.toLowerCase())) {
+        matchQuery.add(item);
+      }
+    }
+    return ListView.builder(
+      itemCount: matchQuery.length,
+      itemBuilder: (context, index) {
+        var result = matchQuery[index];
+        /*
+        return ListTile(
+          title: Text(result),
+        ); */
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    List<String> matchQuery = [];
+    for (var item in allData) {
+      if (item.toLowerCase().contains(query.toLowerCase())) {
+        matchQuery.add(item);
+      }
+    }
+    return ListView.builder(
+      itemCount: matchQuery.length,
+      itemBuilder: (context, index) {
+        var result = matchQuery[index];
+        return ListTile(
+          title: Text(result),
+        );
+      },
+    );
+  }
+}
