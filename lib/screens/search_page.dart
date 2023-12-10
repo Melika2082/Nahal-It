cbbf8022c2d9cfe4bb0455b7ab3c2f2e2c213087
Melika2082 <144673399+Melika2082@users.noreply.github.com>
2023-12-10 17:31:19 +0330

    Add files via upload

--- lib/screens/search_page.dart
+++ lib/screens/search_page.dart
@@ -1,99 +1,95 @@
 import 'package:flutter/material.dart';
+import 'package:nahal_it/home_screen.dart';
 
-class SearchPageTextField extends StatelessWidget {
-  const SearchPageTextField({super.key});
+class SearchPage extends StatefulWidget {
+  const SearchPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Search Page'),
-        actions: [
-          IconButton(
-            onPressed: () {
-              showSearch(
-                context: context,
-                delegate: CustomSearch(),
-              );
-            },
-            icon: const Icon(Icons.search),
-          )
-        ],
-      ),
-      body: const SafeArea(
-        child: Center(),
-      ),
-    );
-  }
+  State<SearchPage> createState() => _SearchPageState();
 }
 
-class CustomSearch extends SearchDelegate {
-  List<String> allData = [
-    'Python',
-    'Flutter',
-    'HTML',
-    'CSS',
-    'Java',
-    'Javascript',
-    'Kotlen',
+class _SearchPageState extends State<SearchPage> {
+  final _controler = PageController();
+  final TextEditingController _searchControler = TextEditingController();
+
+  List<String> filteredData = [];
+  List<String> title = [
+    "اموزش وردپرس",
+    "ثبت سفارش",
+    "پریمر",
+    "طراحی سایت",
+    "اپلیکیشن",
+    "سئو",
+    "ui/ux",
+    "پروشور",
+    "پوستر",
+    "کاتالوگ",
+    "لوگو",
+    "کارت ویزیت",
   ];
 
   @override
-  List<Widget>? buildActions(BuildContext context) {
-    return [
-      IconButton(
-        onPressed: () => query = '',
-        icon: const Icon(Icons.clear),
-      )
-    ];
+  void initState() {
+    super.initState();
+    filteredData = title;
+  }
+
+  void _filterData(String query) {
+    setState(() {
+      filteredData = title
+          .where((item) => item.toLowerCase().contains(query.toLowerCase()))
+          .toList();
+    });
   }
 
   @override
-  Widget? buildLeading(BuildContext context) {
-    return IconButton(
-      onPressed: () {
-        close(context, null);
-      },
-      icon: const Icon(Icons.arrow_back),
-    );
-  }
+  Widget build(BuildContext context) {
+    //  final cart = Provider.of<CartProvider>(context);
+    var size = MediaQuery.of(context).size;
+    return SafeArea(
+      child: Scaffold(
+        //  appBar: newMethod(size),
 
-  @override
-  Widget buildSuggestions(BuildContext context) {
-    List<String> matchQuery = [];
-    for (var item in allData) {
-      if (item.toLowerCase().contains(query.toLowerCase())) {
-        matchQuery.add(item);
-      }
-    }
-    return ListView.builder(
-      itemCount: matchQuery.length,
-      itemBuilder: (context, index) {
-        var result = matchQuery[index];
-        /*
-        return ListTile(
-          title: Text(result),
-        ); */
-      },
-    );
-  }
-
-  @override
-  Widget buildResults(BuildContext context) {
-    List<String> matchQuery = [];
-    for (var item in allData) {
-      if (item.toLowerCase().contains(query.toLowerCase())) {
-        matchQuery.add(item);
-      }
-    }
-    return ListView.builder(
-      itemCount: matchQuery.length,
-      itemBuilder: (context, index) {
-        var result = matchQuery[index];
-        return ListTile(
-          title: Text(result),
-        );
-      },
+        appBar: AppBar(
+          toolbarHeight: 80,
+          title: SizedBox(
+            height: 40,
+            child: TextField(
+              onChanged: _filterData,
+              controller: _searchControler,
+              decoration: const InputDecoration(
+                hintText: 'Search',
+                label: Icon(Icons.search_rounded),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(25),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const HomeScreen(),
+                  ),
+                );
+              },
+              icon: const Icon(Icons.arrow_back),
+            ),
+          ],
+        ),
+        body: ListView.builder(
+            itemCount: filteredData.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(filteredData[index]),
+              );
+            }),
+      ),
     );
   }
 }
